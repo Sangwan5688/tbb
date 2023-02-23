0b161bb2d70d65b8c180d4dd1acdd5015636b66b
Ankit Sangwan <ankit.sa@browserstack.com>
2023-02-23 22:22:59 +0530

    added basic recent posts template

---
+++ data/blogs.js
@@ -0,0 +1,46 @@
+export const blogs = [{
+  title: 'Lorem ipsum dolor sit amet',
+  subtitle: 'Lorem ipsum dolor sit amet',
+  image: 'assets/home_header_laptop.jpg',
+  link: '#',
+}, {
+  title: 'Lorem ipsum dolor sit amet',
+  subtitle: 'Lorem ipsum dolor sit amet',
+  image: 'assets/home_header_laptop.jpg',
+  link: '#',
+}, {
+  title: 'Lorem ipsum dolor sit amet',
+  subtitle: 'Lorem ipsum dolor sit amet',
+  image: 'assets/home_header_laptop.jpg',
+  link: '#',
+}, {
+  title: 'Lorem ipsum dolor sit amet',
+  subtitle: 'Lorem ipsum dolor sit amet',
+  image: 'assets/home_header_laptop.jpg',
+  link: '#',
+}, {
+  title: 'Lorem ipsum dolor sit amet',
+  subtitle: 'Lorem ipsum dolor sit amet',
+  image: 'assets/home_header_laptop.jpg',
+  link: '#',
+}, {
+  title: 'Lorem ipsum dolor sit amet',
+  subtitle: 'Lorem ipsum dolor sit amet',
+  image: 'assets/home_header_laptop.jpg',
+  link: '#',
+}, {
+  title: 'Lorem ipsum dolor sit amet',
+  subtitle: 'Lorem ipsum dolor sit amet',
+  image: 'assets/home_header_laptop.jpg',
+  link: '#',
+}, {
+  title: 'Lorem ipsum dolor sit amet',
+  subtitle: 'Lorem ipsum dolor sit amet',
+  image: 'assets/home_header_laptop.jpg',
+  link: '#',
+}, {
+  title: 'Lorem ipsum dolor sit amet',
+  subtitle: 'Lorem ipsum dolor sit amet',
+  image: 'assets/home_header_laptop.jpg',
+  link: '#',
+}];
---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+import { blogs } from "../data/blogs.js";
+
+const showAd = false;
+
+fetch('templates/card.html')
+  .then(response => response.text())
+  .then(html => {
+    const template = Handlebars.compile(html);
+    for (let i = 0; i < Math.min(blogs.length, 11); i++) {
+      if (i === 0) {
+        blogs[i].header = true;
+      }
+      if ((i === 5 || i === 7) && showAd) {
+        // Will be used to load ad
+        const recentPosts = document.getElementById('recent-posts-section');
+        recentPosts.innerHTML += `<div class="col-lg-3 col-6 mb-4">
+        <div class="card round-element text-center h-100">
+        <div class="card-body">
+        </div>
+        </div>
+      </div>`;
+      }
+      const compiledTemplate = template(blogs[i]);
+      if (i === 0) {
+        const recentPostsLarge = document.getElementById('recent-posts-large');
+        recentPostsLarge.innerHTML = compiledTemplate;
+      } else if (i < 5) {
+        const recentPostsSmall = document.getElementById('recent-posts-small');
+        recentPostsSmall.innerHTML += `<div class="col-lg-6 col-md-6 col-sm-6 col-6 mb-4">${compiledTemplate}</div>`;
+      } else {
+        const recentPosts = document.getElementById('recent-posts-section');
+        recentPosts.innerHTML += `<div class="col-lg-3 col-6 mb-4">${compiledTemplate}</div>`;
+      }
+    };
+  });
