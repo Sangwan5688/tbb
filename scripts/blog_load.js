a1ec6d8b05fdf7f7d0631be13f6407ad4b2b9db8
Ankit Sangwan <ankit.sa@browserstack.com>
2023-02-24 22:17:18 +0530

    added posts header

--- scripts/blog_load.js
+++ scripts/blog_load.js
@@ -1,23 +1,28 @@
 import { showAd } from "/constants/constants.js";
+import { blogs } from "/data/blogs_data.js";
 
 
 
-fetch('/templates/main.html')
+fetch('/templates/main_body.html')
   .then(response => response.text())
   .then(html => {
     const template = Handlebars.compile(html);
-    fetch('/templates/blog.html')
+    fetch('/templates/blog_body.html')
       .then(response => response.text())
       .then(body => {
-        const data = {
+        const templateData = {
+          title: blogs[key].title,
+          subtitle: blogs[key].subtitle,
+          image: blogs[key].image,
           content: body,
         };
-        const compiledTemplate = template(data);
+        const compiledTemplate = template(templateData);
 
         const finalTemplate = Handlebars.compile(compiledTemplate);
         fetch('body.html').then(response => response.text()).then(blog => {
           const data = {
             blog_content: blog,
+            related: blogs[key].related.map((item) => { return blogs[item.key] }),
           };
           const finalCompiledTemplate = finalTemplate(data);
 
