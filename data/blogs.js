cb6576da06be79e02a2bf8703b93fd11daec1592
Ankit Sangwan <ankit.sa@browserstack.com>
2023-02-24 09:14:37 +0530

    changed indexing

--- data/blogs.js
+++ data/blogs.js
@@ -2,12 +2,12 @@ export const blogs = [{
   title: 'Install IPA on iPhone',
   subtitle: 'A Guide Without Jailbreaking',
   image: 'assets/iphone.jpg',
-  link: 'blogs/install_ipa_on_iphone.html',
+  link: 'blogs/install_ipa_on_iphone',
 }, {
   title: 'Arm64 vs Armeabi',
   subtitle: 'Understanding Which App to Download for Your Device',
   image: 'assets/arch_version.png',
-  link: 'blogs/arm64_vs_armeabi.html',
+  link: 'blogs/arm64_vs_armeabi',
 }, {
   title: 'Coming Soon',
   subtitle: 'Coming Soon',
