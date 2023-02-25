e3da9e0befff3717fbe96b3daa7e1a772d1d5664
Ankit Sangwan <ankit.sa@browserstack.com>
2023-02-25 23:01:57 +0530

    replaced pngs with webp

--- data/blogs_data.js
+++ data/blogs_data.js
@@ -2,20 +2,20 @@ export const blogs = {
   install_ipa_on_iphone: {
     title: 'How to install IPA on iPhone',
     subtitle: 'A Guide Without Jailbreaking',
-    image: '/assets/iphone.jpg',
+    image: '/assets/iphone.webp',
     link: '/blogs/install_ipa_on_iphone',
     related: [{ key: 'arm64_vs_armeabi' }],
   },
   arm64_vs_armeabi: {
     title: 'What\'s the difference between Arm64 & Armeabi',
     subtitle: 'Understanding Which App to Download for Your Device',
-    image: '/assets/arch_version.png',
+    image: '/assets/arch_version.webp',
     link: '/blogs/arm64_vs_armeabi',
     related: [{ key: 'install_ipa_on_iphone' }],
   },
   coming_soon: {
     subtitle: 'Coming Soon',
-    image: '/assets/home_header_laptop.jpg',
+    image: '/assets/home_header_laptop.webp',
     link: '#',
     related: [],
   },
