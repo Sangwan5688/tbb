e168ca0baa4404bfa92789e5f008e06d4e286584
Ankit Sangwan <ankit.sa@browserstack.com>
2023-08-15 17:47:06 +0530

    remove extra compilation

--- scripts/blog_load.js
+++ scripts/blog_load.js
@@ -1,31 +1,21 @@
 import { blogs } from "/data/blogs_data.js";
 
-fetch('/templates/main_body.html')
+fetch('/templates/blog_body.html')
   .then(response => response.text())
   .then(html => {
-    const template = Handlebars.compile(html);
-    fetch('/templates/blog_body.html')
-      .then(response => response.text())
-      .then(body => {
-        const templateData = {
-          title: blogs[key].title,
-          subtitle: blogs[key].subtitle,
-          image: blogs[key].image,
-          content: body,
-        };
-        const compiledTemplate = template(templateData);
+    fetch('body.html').then(response => response.text()).then(blog => {
+      const template = Handlebars.compile(html);
 
-        const finalTemplate = Handlebars.compile(compiledTemplate);
-        fetch('body.html').then(response => response.text()).then(blog => {
-          const data = {
-            blog_content: blog,
-            related: blogs[key].related.map((item) => { return blogs[item.key] }),
-          };
-          const finalCompiledTemplate = finalTemplate(data);
+      const templateData = {
+        title: blogs[key].title,
+        subtitle: blogs[key].subtitle,
+        image: blogs[key].image,
+        blog_content: blog,
+        related: blogs[key].related.map((item) => { return blogs[item.key] }),
+      };
+      const compiledTemplate = template(templateData);
 
-          const recentPostsLarge = document.getElementById('body-load');
-          recentPostsLarge.innerHTML = finalCompiledTemplate;
-        });
-      });
-
+      const recentPostsLarge = document.getElementById('body-load');
+      recentPostsLarge.innerHTML = compiledTemplate;
+    });
   });
